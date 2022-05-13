784f0ce41bb3e5b62cc921c077f6ef97bd7deb01
ialbluwi <al.bluwi@gmail.com>
2022-05-13 19:34:30 +0300

    Update ArrayListWithComments.cpp

--- code/extra-material/ArrayListWithComments.cpp
+++ code/extra-material/ArrayListWithComments.cpp
@@ -152,7 +152,7 @@ void ArrayList::shif_left(int start_index)
 // [A | B | C | D | E | F | G |   |   |   ]
 //  0   1   2   3   4   5   6   7   8   9
 //          ^               ^
-//       startIndex	       last
+//       startIndex        last
 //
 // After:
 //      *   *   *   *   *
